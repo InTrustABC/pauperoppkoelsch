f220d2cab8c0ec538587d21aa7dfa25a26eec6f6
Andi <a.hergerts@web.de>
2025-11-16 23:23:22 +0100

    Initial commit

---
+++ src/content.config.ts
@@ -0,0 +1,14 @@
+import { defineCollection, z } from "astro:content";
+import { glob, file } from 'astro/loaders';
+const postsCollection = defineCollection({
+    schema: ({ image }) => z.object({
+        author: z.string(),
+        date: z.string(),
+        image: image(),
+        title: z.string(),
+    })
+})
+
+export const collections = {
+    posts: postsCollection
+}
